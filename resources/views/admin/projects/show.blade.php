9af5cc675ca830e65e790b0c8ee9a50cf4bd91e1
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-11 18:51:58 +0200

    Add button & complete for today

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,8 +6,9 @@
 <section class="clearfix">
     <h2 class="text-muted text-secondary my-5"> {{ $project->slug }} </h2>
     <img src="{{ $project->image }}" alt="" width="300" class="float-start me-3 mb-1" >
-
+    
     <p>{{ $project->text }}</p>
 </section>
+<a href="{{ route('admin.projects.index') }}" class="btn btn-primary my-5"> Torna alla lista </a>
 
 @endsection
