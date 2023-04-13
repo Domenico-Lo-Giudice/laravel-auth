17fc6e5e4c73c13a5aa12ffbe1efce30b59ef243
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-13 16:27:11 +0200

    Add file image selection

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,11 +7,11 @@
     <h2 class="text-muted text-secondary my-5"> {{ $project->slug }} </h2>
 
     <figure>
-        <img src="{{ $project->image }}" alt="" width="300" class="float-start me-3 mb-1" >
+        <img src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->slug }}" width="300" class="float-start me-3 mb-1" >
     </figure>
     
     <p>{{ $project->text }}</p>
 </section>
 <a href="{{ route('admin.projects.index') }}" class="btn btn-primary my-5"> Torna alla lista </a>
 
-@endsection
+@endsection 
